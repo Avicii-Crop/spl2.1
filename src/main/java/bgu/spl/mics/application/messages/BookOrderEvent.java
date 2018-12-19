d0efd0b7d26a231776c0d634111835460b8355ac
ofekshem <ofekshem@post.bgu.ac.il>
2018-12-19 20:48:51 +0200

    check

--- src/main/java/bgu/spl/mics/application/messages/BookOrderEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/BookOrderEvent.java
@@ -3,6 +3,10 @@ package bgu.spl.mics.application.messages;
 import bgu.spl.mics.Event;
 
 public class BookOrderEvent implements Event {
+    private int processTick;
 
-    public BookOrderEvent(){}
+    public BookOrderEvent(int processTick) {
+        this.processTick = processTick;
+    }
+
 }
--- src/main/java/bgu/spl/mics/application/services/APIService.java
+++ src/main/java/bgu/spl/mics/application/services/APIService.java
@@ -1,6 +1,16 @@
 package bgu.spl.mics.application.services;
 
+import bgu.spl.mics.Future;
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.BookStoreRunner;
+import bgu.spl.mics.application.BookStoreRunner.InputJson.OrderSchedule;
+import bgu.spl.mics.application.messages.BookOrderEvent;
+import bgu.spl.mics.application.messages.TickBroadcast;
+import bgu.spl.mics.application.passiveObjects.Customer;
+import bgu.spl.mics.application.passiveObjects.OrderReceipt;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * APIService is in charge of the connection between a client and the store.
@@ -12,16 +22,25 @@ import bgu.spl.mics.MicroService;
  * You MAY change constructor signatures and even add new public constructors.
  */
 public class APIService extends MicroService{
+	private OrderSchedule[] orderSchedule;
+	private Customer cusmtomer;
+	private int currentTick = 0;
 
-	public APIService() {
-		super("Change_This_Name");
-		// TODO Implement this
-	}
-
+	public APIService(String name, OrderSchedule[] orderSchedule, Customer customer) {
+		super(name);
+		this.orderSchedule = orderSchedule;
+		this.cusmtomer = customer;
+		Arrays.sort(orderSchedule);
+			}
 	@Override
 	protected void initialize() {
-		// TODO Implement this
-		
+		int index = 0;
+		subscribeBroadcast(TickBroadcast.class, br -> currentTick=br.getTick());
+			while (orderSchedule[index].getTick() == currentTick) {
+				index++;
+				BookOrderEvent bookOrderEvent = new BookOrderEvent(currentTick);
+				Future orderReceipt = sendEvent(bookOrderEvent);
+			}
+		}
 	}
 
-}
